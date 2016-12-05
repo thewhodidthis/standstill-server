1c4346e10587f4bc3763c2d3d1c6f5f99ae72986
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-12-05 17:23:29 +0200

    fix for eslint

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ class Bipolar extends Transform {
   constructor(seed) {
     super(seed);
 
-    this._store = parseFloat(seed) || 0;
+    this.store = parseFloat(seed) || 0;
   }
 
   _transform(chunk, encoding, callback) {
     const input = parseFloat(chunk);
-    const delta = (input - this._store) * 0.01;
+    const delta = (input - this.store) * 0.01;
 
-    this._store = input;
+    this.store = input;
 
     callback(null, delta.toString());
   }
