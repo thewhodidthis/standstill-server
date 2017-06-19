c4b5f61a65fcb0e702b3881c29987ee2340a68f9
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-19 08:38:30 +0200

    package: cleanup rollup config, drop indentation, source map, rebuild

--- dist/bipolar.js
+++ dist/bipolar.js
@@ -1,34 +1,33 @@
 var bipolar = (function () {
-  'use strict';
+'use strict';
 
-  // Helps report differences
-  var bipolar = function bipolar() {
-    for (var _len = arguments.length, prev = Array(_len), _key = 0; _key < _len; _key++) {
-      prev[_key] = arguments[_key];
+// Helps report differences
+var bipolar = function bipolar() {
+  for (var _len = arguments.length, prev = Array(_len), _key = 0; _key < _len; _key++) {
+    prev[_key] = arguments[_key];
+  }
+
+  // Reset
+  var memo = prev;
+
+  return function () {
+    for (var _len2 = arguments.length, next = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      next[_key2] = arguments[_key2];
     }
 
-    // Reset
-    var memo = prev;
+    // Calculate deltas
+    var diff = memo.map(function (v, i) {
+      return next[i] - v;
+    });
 
-    return function () {
-      for (var _len2 = arguments.length, next = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-        next[_key2] = arguments[_key2];
-      }
+    // Save for later
+    memo = next;
 
-      // Calculate deltas
-      var diff = memo.map(function (v, i) {
-        return next[i] - v;
-      });
-
-      // Save for later
-      memo = next;
-
-      // Array of deltas
-      return diff;
-    };
+    // Array of deltas
+    return diff;
   };
+};
 
-  return bipolar;
+return bipolar;
 
 }());
-//# sourceMappingURL=bipolar.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,13 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'index.es',
-  moduleName: 'bipolar',
   plugins: [
     babel(),
   ],
   targets: [
     {
       format: 'iife',
-      indent: true,
-      sourceMap: true,
+      moduleName: 'bipolar',
       dest: 'dist/bipolar.js'
     },
     {
