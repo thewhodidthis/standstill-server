c47787d7e3fd6009b7179ea9d991ac5e425f0de1
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-23 11:57:42 +0100

    example: rework

--- example.js
+++ example.js
@@ -1,8 +1,24 @@
-import bipolar from './index.mjs'
+'use strict'
 
-const filter = bipolar(0, 0)
+const bender = require('./')
 
-// Log differences in position between clicks
-document.addEventListener('click', ({ x, y }) => {
-  console.log(filter(x, y))
-})
+const random = (x = 100) => Math.floor(Math.random() * x) - (x * 0.5)
+const filter = bender(100)
+
+const repeat = setInterval((runner) => {
+  const data = random()
+
+  console.log('----')
+  console.log(data)
+  console.log(...runner(data))
+}, 200, filter)
+
+const cancel = (timer) => {
+  clearInterval(timer)
+
+  console.log('----')
+}
+
+console.log('Starting from 100, seed with random numbers, log deltas:')
+
+setTimeout(cancel, 1000, repeat)
