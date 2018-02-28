26dc0b4bdbfebae3b61e8ec201d88ee353685344
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-28 10:22:01 +0100

    example: cleanup

--- example.js
+++ example.js
@@ -2,13 +2,13 @@
 
 const bender = require('./')
 
-const random = (x = 100) => Math.floor(Math.random() * x) - (x * 0.5)
+const random = (mid = 100) => Math.floor(Math.random() * mid) - (mid * 0.5)
 const filter = bender(100)
 
 const repeat = setInterval((runner) => {
   const data = random()
 
-  console.log('----')
+  console.log()
   console.log(data)
   console.log(...runner(data))
 }, 200, filter)
@@ -16,7 +16,7 @@ const repeat = setInterval((runner) => {
 const cancel = (timer) => {
   clearInterval(timer)
 
-  console.log('----')
+  console.log()
 }
 
 console.log('Starting from 100, seed with random numbers, log deltas:')
