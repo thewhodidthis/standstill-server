204775f33aed700f0ca9aa1385b6f9f95f6ef4e5
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-11-11 16:46:51 +0000

    test: update per tapeling latest

--- test.js
+++ test.js
@@ -6,13 +6,22 @@ const bender = require('./')
 const filter = bender()
 const result = filter()
 
-equal(typeof filter, 'function', 'returns lamda on init', 'will return')
-ok(Array.isArray(result), 'lamda returns array on call')
+equal
+  .describe('returns lamda on init')
+  .test(typeof filter, 'function')
+
+ok
+  .describe('lamda returns array on call', 'will return')
+  .test(Array.isArray(result))
 
 const i = 1
 const output = bender(0, i)(i, i)
 const [x, y] = output
 
-equal(x, 1, 'is up', 'will compute')
-equal(y, 0, 'is down')
-equal(output.length, 2, 'input/output length is a match')
+equal
+  .describe('is up')
+  .test(x, 1)
+  .describe('is down')
+  .test(y, 0)
+  .describe('input/output length is a match', 'will compute')
+  .test(output.length, 2)
